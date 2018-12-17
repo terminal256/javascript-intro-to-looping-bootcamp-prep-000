5b2147484775f42bba5610d7031bc0e776fad90a
Gregorio Manabat <gregoriomanabat@gmail.com>
2018-12-17 05:36:01 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,10 +1,10 @@
 function forLoop(array){
   for (let i = 0; i < 25; i++){
     //array.push("I am ${i} strange loop${i === 0 ? '' : 's'}")
-    if(i === 0){
-      array.unshift("I am 1 strange loop.");
+    if(i === 1){
+      array.push("I am 1 strange loop.");
     } else {
-      array.unshift("I am ${i} strange loops.");
+      array.push("I am ${i} strange loops.");
     }
   }
   return array;
